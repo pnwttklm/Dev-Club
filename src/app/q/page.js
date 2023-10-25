47e0bc5c6259313e05f2a1b4214cde415587a82c
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2023-10-25 14:07:11 +0700

    Update page.js

--- src/app/q/page.js
+++ src/app/q/page.js
@@ -2,12 +2,12 @@
 import dynamic from "next/dynamic";
 import React from "react";
 
-const qC = dynamic(() => import('../../pages/Index'))
+// const qC = dynamic(() => import('../../pages/Index'))
 
 export default function Home() {
   return (
     <>
-      <qC/>
+      {/* <qC/> */}
     </>
     )
   }
