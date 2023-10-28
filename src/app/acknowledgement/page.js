51eba585d842271df399b3c3fa68729fc52d493c
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2023-10-28 11:44:59 +0700

    1.1.0

--- src/app/acknowledgement/page.js
+++ src/app/acknowledgement/page.js
@@ -3,7 +3,7 @@ export default function acknowledgement() {
         <>
             <div className='flex flex-col justify-items-center justify-self-center my-36 items-center content-center justify-center text-[#1A4789] text-xl'>
                 <h1 className="mb-3 font-extrabold ">Acknowledgement</h1>
-                <h1 className="mt-3 font-bold ">Developers</h1>
+                <h1 className="mt-3 font-bold ">This website has been developed by</h1>
                 <p className="font-regular hover:text-[#AD841F]">Poonyawatt Klumnaim - Faculty of ICT</p>
             </div>
         </>
