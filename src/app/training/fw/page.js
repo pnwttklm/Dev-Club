2a0f0edb2ce8ec7552ab898936f44aea4eaec8a4
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2023-12-18 19:58:49 +0700

    some

--- src/app/training/fw/page.js
+++ src/app/training/fw/page.js
@@ -1,5 +1,6 @@
 export default function FW() {
     return (
-        <iframe src='https://pnwttklm.notion.site/FrontEnd-Website-c1b713568e0941d39134ab3fba746c1e?pvs=4'></iframe>
+        <meta http-equiv="refresh" content="0;url='https://pnwttklm.notion.site/FrontEnd-Website-c1b713568e0941d39134ab3fba746c1e?pvs=4'"></meta>
+        
     )
 }
