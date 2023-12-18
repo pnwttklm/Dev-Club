21f1402003cb153f7fa626cabc5fb692feb9dafc
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2023-12-18 20:03:31 +0700

    Update page.js

--- src/app/training/fw/page.js
+++ src/app/training/fw/page.js
@@ -1,6 +1,6 @@
 export default function FW() {
     return (
-        <meta http-equiv="refresh" content="0;url='https://pnwttklm.notion.site/FrontEnd-Website-c1b713568e0941d39134ab3fba746c1e?pvs=4'"></meta>
+        <meta http-equiv="refresh" content="0;url='https://pnwttklm.notion.site/FrontEnd-Website-Training-Session-892c2c49414c4c088b8edfc39ac15760?pvs=4'"></meta>
         
     )
 }
