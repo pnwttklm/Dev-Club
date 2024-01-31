6c57dd0efd00134225497017ce2ce97d7440f1b1
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2024-01-31 18:20:41 +0700

    1.5.2

---
+++ src/app/tracking/fw/page.tsx
@@ -0,0 +1,5 @@
+export default function FW() {
+    return (
+        <meta http-equiv="refresh" content="0;url='https://pnwttklm.notion.site/FrontEnd-Website-Tracking-System-e5159f62687d441893443ee8b67d307a?pvs=4'"></meta>
+    )
+}
