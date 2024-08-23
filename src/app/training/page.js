e03caabb3bffb5c7aa1ee50d6576554d64adb1c5
Poonyawatt  Klumnaim <poon@pnwttklm.com>
2024-08-23 15:25:10 +0700

    Update page.js

--- src/app/training/page.js
+++ src/app/training/page.js
@@ -1,5 +1,8 @@
 export default function FW() {
     return (
+        <>
         <meta http-equiv="refresh" content="0;url='https://muict-dev-club.notion.site/MUICT-Dev-Club-Training-Hub-11e2f37971bf40b8bf8ac95f56d2006a?pvs=4'"></meta>
+        <h1>Please Wait the website to redirect</h1>
+        </>
     )
 }
