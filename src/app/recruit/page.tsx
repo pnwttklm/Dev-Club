f6154fdc741c790a048c76515d7f6714c873fb01
Poonyawatt Klumnaim <poon@pnwttklm.com>
2025-01-05 15:42:21 +0700

    Update page.tsx

--- src/app/recruit/page.tsx
+++ src/app/recruit/page.tsx
@@ -1,5 +1,11 @@
 export default function FW() {
-    return (
-        <meta http-equiv="refresh" content="0;url='https://forms.gle/1uo6AHk3EPv9Ep6w9'"></meta>
-    )
+  return (
+    <>
+      <meta
+        http-equiv="refresh"
+        content="0;url='https://forms.gle/1uo6AHk3EPv9Ep6w9'"
+      ></meta>
+      <h1>Redirecting...</h1>
+    </>
+  );
 }
