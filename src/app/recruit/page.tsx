1131ba01a33c5507ef2f6169a13f65f49274bd4c
Poonyawatt Klumnaim <poon@pnwttklm.com>
2025-01-26 15:39:33 +0700

    feat: add save image and logo

--- src/app/recruit/page.tsx
+++ src/app/recruit/page.tsx
@@ -4,9 +4,44 @@ import Image from "next/image";
 import { useState } from "react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { Divider } from "@chakra-ui/react";
+import { useRef } from "react";
+import html2canvas from "html2canvas";
+import { BsArrowDownCircleFill } from "react-icons/bs";
 
 export default function HomePage() {
-  const data = GetApplicants();
+  // const data = GetApplicants();
+  const data = [
+    {
+      id: 6688123,
+      nickname: "John",
+      first_team: "bn",
+      teams: ["fw", "fm"],
+      "3-word": "I love coding",
+      anything: null,
+    },
+  ];
+
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  const handleDownload = async () => {
+    if (sectionRef.current) {
+      try {
+        // Capture the section as a canvas
+        const canvas = await html2canvas(sectionRef.current);
+
+        // Convert the canvas to an image (PNG)
+        const dataURL = canvas.toDataURL("image/png");
+
+        // Create a download link
+        const link = document.createElement("a");
+        link.href = dataURL;
+        link.download = "Dev Cub Recruit 2025 " + foundData?.nickname + ".png";
+        link.click();
+      } catch (error) {
+        console.error("Failed to capture and download the image:", error);
+      }
+    }
+  };
 
   const dep = [
     {
@@ -101,77 +136,109 @@ export default function HomePage() {
       return found?.title;
     }
     return (
-      <div className="flex flex-col items-center justify-center gap-8 p-8 bg-gradient-to-b from-[#9C58FD] to-[#74DAFF] text-white rounded-3xl shadow-lg max-w-4xl mx-auto">
-        <DotLottieReact
-          src="tree.lottie"
-          loop
-          autoplay
-          className="w-8/12 sm:w-6/12 mb-6"
-        />
-        <h1 className="text-4xl  text-center">
-          Hi <span className="font-bold">{foundData?.nickname}</span>!
-        </h1>
-        <p className="text-xl font-medium text-center">
-          We hope you&apos;re doing well. Thank you for your interest in the Dev
-          Club! You&apos;re just one step away from becoming a member. To make
-          sure our club aligns with your goals and can support your
-          self-improvement, we would like to schedule an interview. This will be
-          a casual conversation where we get to know you better.
-        </p>
-        <p className="text-xl font-medium text-center">
-          Let&apos;s review your information.
-        </p>
-        <div className="bg-white text-black p-6 rounded-3xl shadow-md mt-6">
-          <div className="items-center justify-center flex flex-col gap-4 mt-12">
-            <p className="text-xl text-center text-black">
-              The team you&apos;re most interested in is:
-            </p>
-            <div className="bg-[#F7F7F7] w-6/12  p-4 rounded-3xl mt-4">
-              <p className="text-center text-2xl font-bold text-yellow-400 mt-2">
-                {getTeamName(foundData?.first_team)}
+      <>
+        <div
+          className="flex flex-col items-center justify-center gap-8 p-8 bg-gradient-to-b from-[#9C58FD] to-[#74DAFF] text-white rounded-3xl shadow-lg max-w-4xl mx-auto"
+          ref={sectionRef}
+        >
+          <div className="items-start w-full">
+            <Image
+              src="logo_ww.svg"
+              alt="hero"
+              className="w-24 h-auto"
+              width={100}
+              height={100}
+            />
+          </div>
+          <DotLottieReact
+            src="tree.lottie"
+            loop
+            autoplay
+            className="w-8/12 sm:w-6/12 mb-6"
+          />
+          <h1 className="text-4xl  text-center">
+            Hi <span className="font-bold">{foundData?.nickname}</span>!
+          </h1>
+          <p className="text-xl font-medium text-center">
+            We hope you&apos;re doing well. Thank you for your interest in the
+            Dev Club! You&apos;re just one step away from becoming a member. To
+            make sure our club aligns with your goals and can support your
+            self-improvement, we would like to schedule an interview. This will
+            be a casual conversation where we get to know you better.
+          </p>
+          <p className="text-xl font-medium text-center">
+            Let&apos;s review your information.
+          </p>
+          <div className="bg-white text-black p-6 rounded-3xl shadow-md w-10/12 mt-6">
+            <div className="items-center justify-center flex flex-col gap-4 mt-12">
+              <p className="text-xl text-center text-black">
+                The team you&apos;re most interested in is:
+              </p>
+              <div className="bg-[#F7F7F7] w-6/12  p-4 rounded-3xl mt-4">
+                <p className="text-center text-2xl font-bold text-yellow-400 mt-2">
+                  {getTeamName(foundData?.first_team)}
+                </p>
+              </div>
+              <p className="text-center text-lg mt-2 text-black">
+                Great choice!
+              </p>
+              <p className="text-center text-black mt-4">
+                You&apos;re also interested in the following teams:
+              </p>
+              <p className="text-center text-lg text-black">
+                {foundData?.teams
+                  .map(getTeamName)
+                  .join(", ")
+                  .replace(/, ([^,]*)$/, " and $1")}
+                .
+              </p>
+              <Divider className="my-12" />
+              <div>
+                <h1 className="text-6xl font-black mr-72">“</h1>
+                <h1 className="text-4xl font-semibold text-center text-green-600">
+                  {foundData?.["3-word"]}
+                </h1>
+                <h1 className="text-6xl font-black ml-72">”</h1>
+              </div>
+              <p className="text-center mt-2">
+                This three-word phrase best describes you.
+              </p>
+              {foundData?.anything && (
+                <>
+                  <p className="text-center text-black">
+                    And you&apos;d like to share the following with us:
+                  </p>
+                  <h3 className="text-2xl font-semibold text-center text-indigo-500 mt-4">
+                    {foundData?.anything}
+                  </h3>
+                </>
+              )}
+              <p className="text-center text-black my-6">
+                We look forward to seeing you at the interview!
               </p>
             </div>
-            <p className="text-center text-lg mt-2 text-black">Great choice!</p>
-            <p className="text-center text-black mt-4">
-              You&apos;re also interested in the following teams:
-            </p>
-            <p className="text-center text-lg text-black">
-              {foundData?.teams
-                .map(getTeamName)
-                .join(", ")
-                .replace(/, ([^,]*)$/, " and $1")}
-              .
-            </p>
-            <Divider className="my-12" />
-            <div>
-              <h1 className="text-6xl font-black mr-72">“</h1>
-              <h1 className="text-4xl font-semibold text-center text-green-600">
-                {foundData?.["3-word"]}
-              </h1>
-              <h1 className="text-6xl font-black ml-72">”</h1>
-            </div>
-            <p className="text-center mt-2">
-              This three-word phrase best describes you.
-            </p>
-            <p className="text-center text-black">
-              And you&apos;d like to share the following with us:
-            </p>
-            <h3 className="text-2xl font-semibold text-center text-indigo-500 mt-4">
-              {foundData?.anything}
-            </h3>
-            <p className="text-center text-black my-6">
-              We look forward to seeing you at the interview!
-            </p>
+          </div>
+
+          <button
+            className="mt-8 px-6 py-3 bg-[#001C26] hover:bg-green-700 text-white font-semibold rounded-full transition-all ease-in-out duration-200"
+            onClick={() => setPage(2)}
+          >
+            Schedule the Interview
+          </button>
+          <div className="text-right w-full">
+            <p>{foundData?.id}</p>
           </div>
         </div>
-
-        <button
-          className="mt-8 px-6 py-3 bg-[#001C26] hover:bg-green-700 text-white font-semibold rounded-full transition-all ease-in-out duration-200"
-          onClick={() => setPage(2)}
-        >
-          Schedule the Interview
-        </button>
-      </div>
+        <div className="flex flex-col items-center justify-center">
+          <button
+            onClick={handleDownload}
+            className="mt-6 px-4 py-2 flex flex-row gap-3 items-center  bg-[#9C58FD] text-white hover:text-[#001C26] hover:border-[#001C26] hover:border-2 rounded-full hover:bg-white transition duration-300"
+          >
+            <BsArrowDownCircleFill />
+            <p>Download as Image</p>
+          </button>
+        </div>
+      </>
     );
   }
 
@@ -195,7 +262,8 @@ export default function HomePage() {
           className="w-8/12 sm:w-6/12 mb-6"
         />
         <p className="text-lg font-semibold text-red-600">
-          No Application Found for Student ID: <span className="font-bold">{id}</span>
+          No Application Found for Student ID:{" "}
+          <span className="font-bold">{id}</span>
         </p>
         <button
           onClick={() => setPage(0)}
